f367af0ecdd19198e04b2a28ee3aec26a625605f
nbsteuv <nbsteuv@gmail.com>
2017-01-17 08:03:40 -0500

    Added game controller object and random word selection

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour {
+
+    public TextAsset threeWordList;
+    public TextAsset fourWordList;
+
+	// Use this for initialization
+	void Start () {
+        string word = getRandomWordFromList(threeWordList);
+        Debug.Log(word);
+        string secondWord = getRandomWordFromList(fourWordList);
+        Debug.Log(secondWord);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    string getRandomWordFromList(TextAsset wordList)
+    {
+        string[] wordArray = getArrayFromWordList(wordList);
+        string randomWord = getRandomWordFromArray(wordArray);
+        return randomWord;
+    }
+
+    string[] getArrayFromWordList(TextAsset wordList)
+    {
+        string[] wordArray = wordList.text.Split("\n"[0]);
+        return wordArray;
+    }
+
+    string getRandomWordFromArray(string[] wordArray)
+    {
+        int randomIndex = Random.Range(0, wordArray.Length);
+        string randomWord = wordArray[randomIndex].Trim();
+        return randomWord;
+    }
+
+}

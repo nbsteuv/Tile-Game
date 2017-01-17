d04127fe344803cff5dbde17acc65613ad867f11
nbsteuv <nbsteuv@gmail.com>
2017-01-17 07:23:24 -0500

    Tile spawns tile text with empty string on start

---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour {
+
+    public GameObject tileTextPrefab;
+
+	// Use this for initialization
+	void Start () {
+        GameObject tileText = (GameObject)Instantiate(tileTextPrefab, transform.position, Quaternion.identity);
+        TextMesh tileTextMesh = tileText.GetComponent<TextMesh>();
+        tileTextMesh.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

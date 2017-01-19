792caea9080c1b86efa16782a7f1cd8460cfd1a8
nbsteuv <nbsteuv@gmail.com>
2017-01-18 20:23:04 -0500

    gameWord list built in game controller script

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -7,18 +7,53 @@ public class GameScript : MonoBehaviour {
     public TextAsset threeWordList;
     public TextAsset fourWordList;
 
+    List<string> gameWords = new List<string>();
+
 	// Use this for initialization
 	void Start () {
-        string word = getRandomWordFromList(threeWordList);
-        Debug.Log(word);
-        string secondWord = getRandomWordFromList(fourWordList);
-        Debug.Log(secondWord);
+        setGameWords();
+        for(int i = 0; i < gameWords.Count; i++)
+        {
+            Debug.Log(gameWords[i]);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void setGameWords()
+    {
+        //TODO: Replace i and wordList values based on selected number of squares 
+        for(int i = 0; i < 3; i++)
+        {
+            setUniqueWord(fourWordList);
+        }
+        setUniqueWord(threeWordList);
+    }
+
+    void setUniqueWord(TextAsset wordList)
+    {
+        bool gameWordInserted = false;
+        while (!gameWordInserted)
+        {
+            string newWord = getRandomWordFromList(wordList);
+            gameWordInserted = insertGamewordIfUnique(newWord);
+        }
+    }
+
+    bool insertGamewordIfUnique(string word)
+    {
+        if (gameWords.Contains(word))
+        {
+            return false;
+        } else
+        {
+            gameWords.Add(word);
+            return true;
+        }
+    }
 
     string getRandomWordFromList(TextAsset wordList)
     {

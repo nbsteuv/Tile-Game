38b1a3c8d9f8f6b297762aa54cab00de95edf78d
nbsteuv <nbsteuv@gmail.com>
2017-01-24 20:14:16 -0500

    Created timer prefab with timer script

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerScript : MonoBehaviour {
+
+    decimal timer = 0.00M;
+    bool timerActive = false;
+    TextMesh textMesh;
+
+	// Use this for initialization
+	void Start () {
+        textMesh = gameObject.GetComponent<TextMesh>();
+        textMesh.text = "Time\n" + timer.ToString();
+        startTimer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (timerActive)
+        {
+            timer += Math.Round((decimal)Time.deltaTime,2);
+            textMesh.text = "Time\n" + timer.ToString();
+        }
+	}
+
+    public void startTimer()
+    {
+        timerActive = true;
+    }
+
+    public void stopTimer()
+    {
+        timerActive = false;
+    }
+}

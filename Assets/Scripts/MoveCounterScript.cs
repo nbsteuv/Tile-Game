623728921ce4bdc582888059f649eabd3ac82c34
nbsteuv <nbsteuv@gmail.com>
2017-01-25 05:28:16 -0500

    Added move counter with script

---
+++ Assets/Scripts/MoveCounterScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCounterScript : MonoBehaviour {
+
+    public int moves = 0;
+    TextMesh textMesh;
+
+	// Use this for initialization
+	void Start () {
+        textMesh = gameObject.GetComponent<TextMesh>();
+        textMesh.text = "Moves\n" + moves.ToString();
+        incrementMoves();
+        incrementMoves();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void incrementMoves()
+    {
+        moves++;
+        textMesh.text = "Moves\n" + moves.ToString();
+    }
+}
